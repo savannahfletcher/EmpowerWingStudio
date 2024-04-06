9e7b52cb0a91aa33fdca89823ea91366c3cb1ccf
Savannah Fletcher <sfletcher777@gmail.com>
2024-04-05 22:49:30 -0400

    Added Navbar and Routing

---
+++ empowerwing-studio/src/Pages/Apply/Apply.tsx
@@ -0,0 +1,13 @@
+import React, { Component, useState } from 'react';
+import "./Apply.css"; 
+
+
+const Apply = () => {
+
+    return(
+        <div className="apply">
+        </div>
+    )
+};
+
+export default Apply; 
---
+++ empowerwing-studio/src/Pages/Showcase/Showcase.tsx
@@ -0,0 +1,12 @@
+import React, { Component, useState } from 'react';
+import "./Showcase.css";
+
+const Showcase = () => {
+
+    return(
+        <div className="home">
+        </div>
+    )
+};
+
+export default Showcase; 
