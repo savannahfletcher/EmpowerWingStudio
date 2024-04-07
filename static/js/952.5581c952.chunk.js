dac797ca4fd97de7ee734cd69cd5c25f63042a1d
Savannah Fletcher <sfletcher777@gmail.com>
2024-04-07 06:55:41 -0400

    Updates

---
+++ static/js/952.5581c952.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkempowerwing_studio=self.webpackChunkempowerwing_studio||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.5581c952.chunk.js.map
