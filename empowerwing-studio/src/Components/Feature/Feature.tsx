62c7d329b09bb2c61d1f62c98e53f3582e194c47
Savannah Fletcher <sfletcher777@gmail.com>
2024-04-06 20:18:58 -0400

    Artist feature rendering

--- empowerwing-studio/src/Components/Feature/Feature.tsx
+++ empowerwing-studio/src/Components/Feature/Feature.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./Feature.css"; 
+import "./Feature.css";
 
 interface User {
     id: number;
@@ -15,17 +15,41 @@ interface FeatureProps {
     user: User;
 }
 
-const Feature : React.FC<FeatureProps> = ({user}) => {
-    return(
-        <div className = "feature">
+const Feature: React.FC<FeatureProps> = ({ user }) => {
+    return (
+        <div className="feature">
             <div className="flex-container">
                 <div className="flex-item">
-                    <h3 style={{color:'white'}}>{user.name}</h3>
+                    {/* <h3 style={{color:'white'}}>{user.name}</h3>
                     <p style={{color:'white'}}>{user.biography}</p>
-                    <p style={{color:'white'}}>{user.link1}</p>
+                    <p style={{color:'white'}}>{user.link1}</p> */}
+                    {/* Left half of feature: profile, name, bio */}
+                    <div className="left-box">
+                        <div className="upper">
+                            <div className="circle"></div>
+                            <div className='name-text'>{user.name}</div>
+                        </div>
+                        <div className="lower">
+                            <div className='bio-text'>{user.biography}</div>
+                            <div className='link-text'>{user.link1}</div>
+                            {user.link2 !== '' &&
+                                <div className='link-text'>{user.link2}</div>
+
+                            }
+                            {user.link3 !== '' &&
+                                <div className='link-text'>{user.link3}</div>
+                            }
+
+                        </div>
+
+                    </div>
+                    {/* Right half of feature: artwork images */}
+                    <div className="right-box">
+
+
+                    </div>
+
                 </div>
-                    
-                <div className="flex-item">c 1</div>
             </div>
         </div>
     )
